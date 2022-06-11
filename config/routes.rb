4e589b1c15a2fda3379af9fde127188a25f445b4
philipp galbo <pjgalbo@gmail.com>
2022-06-11 15:44:29 +1000

    lists controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'lists', to: 'lists#index'
+  post 'lists', to: 'lists#create'
+  get 'lists/new', to: 'lists#new'
+  get 'lists/:id', to: 'lists#show'
+
+
+
+
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete 'tasks/:id', to: 'tasks#destroy'
+
 end
